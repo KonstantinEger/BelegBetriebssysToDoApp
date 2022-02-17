336380ef08650bf5b9503991dd685ef4fd6dea27
KonstantinEger <35273325+KonstantinEger@users.noreply.github.com>
2022-02-17 16:56:10 +0100

    load own html

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 const {app, BrowserWindow} = require("electron");
+const path = require("path");
+const url = require("url");
 
 app.on("ready", () => {
     const win = new BrowserWindow({
         width: 600,
         height: 900
     });
-    win.loadURL("https://www.google.com/");
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, "app/index.html"),
+        protocol: "file",
+        slashes: true
+    }));
 });
